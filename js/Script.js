f2d248f81823c71af53e92ecdd0bb74c4ded48a6
Zack <zack4@live.ca>
2020-02-16 18:03:48 -0500

    JS Slide vers section

--- js/Script.js
+++ js/Script.js
@@ -27,4 +27,49 @@ function menuActif(){
 
 }
 
+btnHeader = document.getElementById('btnAccueil');
+btnHeader.addEventListener('click', smoothScrollAccueil)
+
+
+function smoothScrollAccueil () {
+  document.querySelector('#headerLink').scrollIntoView({
+        behavior: 'smooth'
+      });
+  }
+
+
+
+  btnMusique = document.getElementById('btnMusique');
+  btnMusique.addEventListener('click', smoothScrollMusique)
+  
+  
+  function smoothScrollMusique () {
+    document.querySelector('#sectionMusique').scrollIntoView({
+          behavior: 'smooth'
+        });
+    }
+
+      
+
+  btnPropos = document.getElementById('btnPropos');
+  btnPropos.addEventListener('click', smoothScrollPropos)
+  
+  
+  function smoothScrollPropos () {
+    document.querySelector('#aPropos').scrollIntoView({
+          behavior: 'smooth'
+        });
+    }
+
+      
+
+  btnStore = document.getElementById('btnStore');
+  btnStore.addEventListener('click', smoothScrollStore)
+  
+  
+  function smoothScrollStore () {
+    document.querySelector('#store').scrollIntoView({
+          behavior: 'smooth'
+        });
+    }
 
