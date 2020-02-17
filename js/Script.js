15821ff025e64b255f1bfdc32bed336702b2cb51
Zack <zack4@live.ca>
2020-02-16 23:57:22 -0500

    menu burger

--- js/Script.js
+++ js/Script.js
@@ -5,25 +5,22 @@ btnMenu.addEventListener('click', menuActif)
 function menuActif(){
     //bouton actif
 
-
     chkboxMenu.checked = true;
 
-//    document.getElementById('fondBlanc').style.transform = "translate(0%, 0%)";
+
+    document.getElementById('fondBlanc').style.transform = "translate(0%, 0%)";
+    document.getElementById('burger').style.transform = "translate(0%, 0%)";
     
-  //  document.getElementById('nav').style.transform = "translate(0%, -9%)";
+    if (window.innerWidth > 600) {
+    //if width > 600
+    document.getElementById('nav').style.transform = "translate(0%, 0%)";
+    }
 
-
-    //if(window.innerWidth < 600){
-     //   document.getElementById('containBurger').style.left = "0%"
-    //}
-
- //   if(window.innerWidth >= 600){
-  //  document.getElementById('containBurger').style.transform = "translate(0%, 0%)";
-   // document.getElementById('containBurger').style.left = "0%"
-
-   // }
-
-
+    if (window.innerWidth < 600) {
+    //if width < 600
+    document.getElementById('nav').style.transform = "translate(0%, -6%)";
+      behavior: 'smooth';
+  }
 
 }
 
